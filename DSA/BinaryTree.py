29e650e24ca2e1dc04b31ba4e8cfee94f9b952b4
Yashkalra12 <yash1578.be21@chitkara.edu.in>
2024-04-16 20:32:02 +0530

    Binary Tree

--- DSA/BinaryTree.py
+++ DSA/BinaryTree.py
@@ -18,7 +18,22 @@ class BinarySearchTreeNode:
                 self.right.add_child(data)
             else:
                 self.right=BinarySearchTreeNode(data)            
+    def find_min(self):
+        if self.left is None:
+            return self.data
+        return self.left.find_min()
+    
+    def find_max(self):
+        if self.right is None:
+            return self.data
+        return self.right.find_max()
+    
+    def calculate_sum(self):
+        left_sum= self.left.calculate_sum() if self.left else 0
+        right_sum= self.right.calculate_sum() if self.right else 0
 
+        return self.data + left_sum + right_sum
+    
     def in_order_traversal(self):
         elements=[]
 
@@ -32,6 +47,32 @@ class BinarySearchTreeNode:
         #Visit right node
         if self.right:
             elements+=self.right.in_order_traversal()
+
+        return elements
+    
+    def pre_order_traversal(self):
+        elements=[]
+        #Visit     
+        elements.append(self.data)
+        #Visit left node
+        if self.left:
+            elements+=self.left.pre_order_traversal()
+        #Visit right node
+        if self.right:
+            elements+=self.right.pre_order_traversal()
+
+        return elements
+    
+    def post_order_traversal(self):
+        elements=[]
+        #Visit left node
+        if self.left:
+            elements+=self.left.post_order_traversal()
+        #Visit right node
+        if self.right:
+            elements+=self.right.post_order_traversal()
+        #Visit     
+        elements.append(self.data)
 
         return elements
     
@@ -59,5 +100,10 @@ def build_tree(elements):
 if __name__=='__main__':
     numbers=[17,4,1,20,9,23,18,34]
     numbers_tree= build_tree(numbers)
-    print(numbers_tree.in_order_traversal())
+    print(f'InOrder: {numbers_tree.in_order_traversal()}')
+    print(f'PreOrder: {numbers_tree.pre_order_traversal()}')
+    print(f'PostOrder: {numbers_tree.post_order_traversal()}')
+    print(f'Minimum_Value: {numbers_tree.find_min()}')
+    print(f'Maximum_Value: {numbers_tree.find_max()}')
+    print(f'Sum: {numbers_tree.calculate_sum()}')
     print(numbers_tree.search(9))
